7138c27123f303fd832f2c3f14f27f05001977c6
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2023-10-05 10:26:51 +0900

    誤字の修正
    クラス名とファイル名の不一致、for文の記法ミスを修正しました。

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
-public class Pra2 {
+public class pra2 {
 	public static void main(String[] args) {
-		for(int i = 0, i < 100; i++) {
+		for(int i = 0; i < 100; i++) {
 			System.out.println("Hello World");
 		}
 	}
